10fe0d3cf5b1c6559a75d732c8bbfd3896c4145f
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2024-06-13 14:41:31 +0700

    [ADD] Firebase config for frontend

---
+++ frontend/src/utils/errorMessage.ts
@@ -0,0 +1,20 @@
+interface FirebaseError {
+    message: string;
+    status: string;
+  }
+  
+export const extractFirebaseError = (text: string): FirebaseError | null => {
+    const regex = /HTTP error \d{3}: (\{.*\})\)\)/;
+    const match = text.match(regex);
+  
+    if (!match || !match[1]) {
+        return null;
+    }
+  
+    const errorObject = JSON.parse(match[1]);
+    
+    return {
+      message: errorObject.error.message,
+      status: errorObject.error.status
+    };
+  }
